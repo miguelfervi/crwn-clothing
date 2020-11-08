fbf81765a2f86da426bc93a1099dd55da1f56993
Miguel <miguelfervi@gmail.com>
2020-11-08 13:28:47 +0100

    integrated firebase

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header></Header>
+        <Header currentUser={this.state.currentUser}></Header>
         <Switch>
           <Route exact path='/' component={HomePage}></Route>
           <Route path='/shop' component={ShopPage}></Route>
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,10 +47,12 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
             label='password'
             required></FormInput>
-          <CustomButton type='submit'>Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign in with Google
-          </CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'>Sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
