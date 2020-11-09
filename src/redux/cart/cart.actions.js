1e31479fc68ae7959ed9b8a605a7422759a17f92
Miguel <miguelfervi@gmail.com>
2020-11-09 16:52:56 +0100

    ADD cart

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,6 @@
+import CartActionTypes from './cart.types';
+import CartActionsTypes from './cart.types';
+
+export const toggleCartHidden = () => ({
+  type: CartActionsTypes.TOOGLE_CART_HIDDEN,
+});
